f2fa4031e152a93b604324e1e8b13026f288c521
Venkata Penumatsa <penumatv@gmail.com>
2022-06-15 20:42:05 +0530

    Header Change

--- components/layouts/Header.js
+++ components/layouts/Header.js
@@ -14,8 +14,6 @@ import {
   useColorModeValue,
   useBreakpointValue,
   useDisclosure,
-  HStack,
-  Heading,
 } from "@chakra-ui/react";
 import {
   HamburgerIcon,
@@ -24,64 +22,26 @@ import {
   ChevronRightIcon,
 } from "@chakra-ui/icons";
 
-// import NextLink from "next/link";
-import { ColorModeSwitcher } from "./ColorModeSwitcher";
-import { AiFillCode } from "react-icons/ai";
-
-export default function Header() {
-  const { isOpen, onClose, onToggle } = useDisclosure();
+export default function WithSubnavigation() {
+  const { isOpen, onToggle } = useDisclosure();
 
   return (
-    <Box bg={useColorModeValue("gray.100", "gray.900")} px={4}>
+    <Box>
       <Flex
-        // bg={useColorModeValue("white", "gray.800")}
-        // color={useColorModeValue("gray.600", "white")}
+        bg={useColorModeValue("white", "gray.800")}
+        color={useColorModeValue("gray.600", "white")}
         minH={"60px"}
-        py={{ base: 1 }}
-        px={{ base: 1 }}
+        py={{ base: 2 }}
+        px={{ base: 4 }}
         borderBottom={1}
         borderStyle={"solid"}
         borderColor={useColorModeValue("gray.200", "gray.900")}
         align={"center"}
       >
-        <Flex flex={{ base: 1 }} justify={{ base: "center", md: "start" }}>
-          <HStack spacing="0px">
-            <AiFillCode size={24} />
-            {/* <NextLink
-              href={{
-                pathname: "/",
-              }}
-              passHref
-            >
-              <Link>
-                <Text
-                  fontSize="2xl"
-                  textAlign={useBreakpointValue({ base: "center", md: "left" })}
-                  fontFamily={"heading"}
-                  color={useColorModeValue("gray.800", "white")}
-                >
-                  My&nbsp;Academy
-                </Text>
-              </Link>
-            </NextLink> */}
-          </HStack>
-        </Flex>
-        <Stack
-          flex={{ base: 1, md: 5 }}
-          justify={"flex-end"}
-          direction={"row"}
-          spacing={2}
-        >
-          <Flex display={{ base: "none", md: "flex" }} ml={20}>
-            {/* <DesktopNav /> */}
-          </Flex>
-        </Stack>
-        <ColorModeSwitcher />
-
         <Flex
-          // flex={{ base: 1, md: "auto" }}
-          // ml={{ base: -2 }}
-          display={{ base: "end", md: "none" }}
+          flex={{ base: 1, md: "auto" }}
+          ml={{ base: -2 }}
+          display={{ base: "flex", md: "none" }}
         >
           <IconButton
             onClick={onToggle}
@@ -92,6 +52,49 @@ export default function Header() {
             aria-label={"Toggle Navigation"}
           />
         </Flex>
+        <Flex flex={{ base: 1 }} justify={{ base: "center", md: "start" }}>
+          <Text
+            textAlign={useBreakpointValue({ base: "center", md: "left" })}
+            fontFamily={"heading"}
+            color={useColorModeValue("gray.800", "white")}
+          >
+            Logo
+          </Text>
+
+          <Flex display={{ base: "none", md: "flex" }} ml={10}>
+            <DesktopNav />
+          </Flex>
+        </Flex>
+
+        <Stack
+          flex={{ base: 1, md: 0 }}
+          justify={"flex-end"}
+          direction={"row"}
+          spacing={6}
+        >
+          <Button
+            as={"a"}
+            fontSize={"sm"}
+            fontWeight={400}
+            variant={"link"}
+            href={"#"}
+          >
+            Sign In
+          </Button>
+          <Button
+            display={{ base: "none", md: "inline-flex" }}
+            fontSize={"sm"}
+            fontWeight={600}
+            color={"white"}
+            bg={"pink.400"}
+            href={"#"}
+            _hover={{
+              bg: "pink.300",
+            }}
+          >
+            Sign Up
+          </Button>
+        </Stack>
       </Flex>
 
       <Collapse in={isOpen} animateOpacity>
@@ -102,9 +105,9 @@ export default function Header() {
 }
 
 const DesktopNav = () => {
-  // const linkColor = useColorModeValue("gray.600", "gray.200");
-  // const linkHoverColor = useColorModeValue("gray.800", "white");
-  // const popoverContentBgColor = useColorModeValue("white", "gray.800");
+  const linkColor = useColorModeValue("gray.600", "gray.200");
+  const linkHoverColor = useColorModeValue("gray.800", "white");
+  const popoverContentBgColor = useColorModeValue("white", "gray.800");
 
   return (
     <Stack direction={"row"} spacing={4}>
@@ -112,19 +115,78 @@ const DesktopNav = () => {
         <Box key={navItem.label}>
           <Popover trigger={"hover"} placement={"bottom-start"}>
             <PopoverTrigger>
-              <NextLink
-                href={{
-                  pathname: navItem.navigate_to,
+              <Link
+                p={2}
+                href={navItem.href ?? "#"}
+                fontSize={"sm"}
+                fontWeight={500}
+                color={linkColor}
+                _hover={{
+                  textDecoration: "none",
+                  color: linkHoverColor,
                 }}
-                passHref
               >
-                <Link>{navItem.label}</Link>
-              </NextLink>
+                {navItem.label}
+              </Link>
             </PopoverTrigger>
+
+            {navItem.children && (
+              <PopoverContent
+                border={0}
+                boxShadow={"xl"}
+                bg={popoverContentBgColor}
+                p={4}
+                rounded={"xl"}
+                minW={"sm"}
+              >
+                <Stack>
+                  {navItem.children.map((child) => (
+                    <DesktopSubNav key={child.label} {...child} />
+                  ))}
+                </Stack>
+              </PopoverContent>
+            )}
           </Popover>
         </Box>
       ))}
     </Stack>
+  );
+};
+
+const DesktopSubNav = ({ label, href, subLabel }) => {
+  return (
+    <Link
+      href={href}
+      role={"group"}
+      display={"block"}
+      p={2}
+      rounded={"md"}
+      _hover={{ bg: useColorModeValue("pink.50", "gray.900") }}
+    >
+      <Stack direction={"row"} align={"center"}>
+        <Box>
+          <Text
+            transition={"all .3s ease"}
+            _groupHover={{ color: "pink.400" }}
+            fontWeight={500}
+          >
+            {label}
+          </Text>
+          <Text fontSize={"sm"}>{subLabel}</Text>
+        </Box>
+        <Flex
+          transition={"all .3s ease"}
+          transform={"translateX(-10px)"}
+          opacity={0}
+          _groupHover={{ opacity: "100%", transform: "translateX(0)" }}
+          justify={"flex-end"}
+          align={"center"}
+          flex={1}
+        >
+          <Icon color={"pink.400"} w={5} h={5} as={ChevronRightIcon} />
+        </Flex>
+      </Stack>
+    </Link>
   );
 };
 
@@ -142,46 +204,96 @@ const MobileNav = () => {
   );
 };
 
-const MobileNavItem = ({ label, children, navigate_to }) => {
+const MobileNavItem = ({ label, children, href }) => {
   const { isOpen, onToggle } = useDisclosure();
 
   return (
     <Stack spacing={4} onClick={children && onToggle}>
       <Flex
         py={2}
+        as={Link}
+        href={href ?? "#"}
         justify={"space-between"}
         align={"center"}
         _hover={{
           textDecoration: "none",
         }}
       >
-        {/* <NextLink
-          href={{
-            pathname: navigate_to,
-          }}
-          passHref
+        <Text
+          fontWeight={600}
+          color={useColorModeValue("gray.600", "gray.200")}
         >
-          <Link> {label} </Link>
-        </NextLink> */}
+          {label}
+        </Text>
+        {children && (
+          <Icon
+            as={ChevronDownIcon}
+            transition={"all .25s ease-in-out"}
+            transform={isOpen ? "rotate(180deg)" : ""}
+            w={6}
+            h={6}
+          />
+        )}
       </Flex>
+
+      <Collapse in={isOpen} animateOpacity style={{ marginTop: "0!important" }}>
+        <Stack
+          mt={2}
+          pl={4}
+          borderLeft={1}
+          borderStyle={"solid"}
+          borderColor={useColorModeValue("gray.200", "gray.700")}
+          align={"start"}
+        >
+          {children &&
+            children.map((child) => (
+              <Link key={child.label} py={2} href={child.href}>
+                {child.label}
+              </Link>
+            ))}
+        </Stack>
+      </Collapse>
     </Stack>
   );
 };
 
-// interface NavItem {
-//   label: string;
-//   subLabel?: string;
-//   children?: Array<NavItem>;
-//   href?: string;
-// }
-
 const NAV_ITEMS = [
   {
-    label: "Sign In",
-    navigate_to: "/sign-in",
+    label: "Inspiration",
+    children: [
+      {
+        label: "Explore Design Work",
+        subLabel: "Trending Design to inspire you",
+        href: "#",
+      },
+      {
+        label: "New & Noteworthy",
+        subLabel: "Up-and-coming Designers",
+        href: "#",
+      },
+    ],
   },
   {
-    label: "Sign Up",
-    navigate_to: "/sign-up",
+    label: "Find Work",
+    children: [
+      {
+        label: "Job Board",
+        subLabel: "Find your dream design job",
+        href: "#",
+      },
+      {
+        label: "Freelance Projects",
+        subLabel: "An exclusive list for contract work",
+        href: "#",
+      },
+    ],
+  },
+  {
+    label: "Learn Design",
+    href: "#",
+  },
+  {
+    label: "Hire Designers",
+    href: "#",
   },
 ];
