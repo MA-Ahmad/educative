925c898cafeab8dd7f75a29f2dfc94d2fff5df77
Muhammad Ahmad <muhammad.ahmad8043@gmail.com>
2022-06-24 12:16:52 +0500

    Chore: Add maxW prop to card

--- components/Card/CourseCard.js
+++ components/Card/CourseCard.js
@@ -8,7 +8,6 @@ import {
   Stack,
   Text,
   Avatar,
-  chakra,
   Button,
   HStack,
   Skeleton,
@@ -16,7 +15,6 @@ import {
   useBreakpointValue,
   useColorModeValue,
 } from '@chakra-ui/react'
-import { PriceTag } from './PriceTag'
 import NextLink from 'next/link'
 import { FaStar } from 'react-icons/fa'
 import { BsClock, BsBarChartLine } from 'react-icons/bs'
@@ -30,106 +28,96 @@ const CourseCard = (props) => {
   const textColor = useColorModeValue('gray.600', 'gray.400')
 
   return (
-    <div>
-      <Stack
-        spacing={useBreakpointValue({
-          base: '4',
-          md: '5',
-        })}
-        {...rootProps}
-      >
-        <Box position="relative">
-          <AspectRatio ratio={4 / 2}>
-            <Image
-              src={imageUrl}
-              alt={name}
-              draggable="false"
-              fallback={<Skeleton />}
-              borderRadius={useBreakpointValue({
-                base: 'md',
-                md: 'xl',
-              })}
-            />
-          </AspectRatio>
-          <Box
-            position="absolute"
-            top="3"
-            left="3"
-            p={1.5}
-            bg="yellow.400"
-            rounded="md"
-          >
-            <Text fontSize="xs" lineHeight={1}>
-              ABCD
-            </Text>
-          </Box>
+    <Stack spacing={3} {...rootProps} maxW={{ base: 'auto', md: '17rem' }}>
+      <Box position="relative">
+        <AspectRatio ratio={4 / 2}>
+          <Image
+            src={imageUrl}
+            alt={name}
+            draggable="false"
+            fallback={<Skeleton />}
+            borderRadius={useBreakpointValue({
+              base: 'md',
+              md: 'xl',
+            })}
+          />
+        </AspectRatio>
+        <Box
+          position="absolute"
+          top="3"
+          left="3"
+          p={1.5}
+          bg="yellow.400"
+          rounded="md"
+        >
+          <Text fontSize="xs" lineHeight={1}>
+            ABCD
+          </Text>
         </Box>
+      </Box>
 
-        {/* Course name and Price  */}
-        <Stack>
-          <Stack spacing="1" direction="row" justify="space-between">
-            <Text fontWeight="medium" color={textColor}>
-              {name}
-            </Text>
-            <PriceTag price={price} currency="USD" />
-          </Stack>
-          <Stack
-            spacing="2"
-            direction="row"
-            alignItems="center"
-            justifyContent="center"
+      {/* Course name and Price  */}
+      <Stack spacing={2}>
+        <Stack spacing="1" direction="row" justify="space-between">
+          <Text
+            fontWeight="medium"
+            fontSize="sm"
+            color={textColor}
+            noOfLines={2}
           >
-            <Avatar src="" size="sm" />
+            {name}
+          </Text>
+        </Stack>
+        <Stack spacing="2" direction="row" alignItems="center">
+          <Avatar src="" size="sm" />
 
-            <Text fontWeight="medium" color={textColor}>
-              Trainer Name
-            </Text>
-          </Stack>
+          <Text fontWeight="medium" color={textColor}>
+            Trainer Name
+          </Text>
+        </Stack>
 
-          {/* Star (total  Reviews) | Total enrollments */}
-          <Stack
-            direction={{ base: 'column', sm: 'row' }}
-            justifyContent="center"
-            alignItems="center"
-          >
-            <HStack>
-              <Icon as={FaStar} color="yellow.400" />
-              <Text fontSize="sm" color={textColor}>
-                4.4 (12 Reviews)
-              </Text>
-            </HStack>
-            <VerticalDivider
-              height="15px"
-              display={{ base: 'none', sm: 'block' }}
-            />
+        {/* Star (total  Reviews) | Total enrollments */}
+        <Stack
+          direction={{ base: 'column', sm: 'row' }}
+          alignItems={{ base: 'flex-start', sm: 'center' }}
+        >
+          <HStack>
+            <Icon as={FaStar} color="yellow.400" />
             <Text fontSize="sm" color={textColor}>
-              100 Enrollments
+              4.4 (12 Reviews)
             </Text>
-          </Stack>
+          </HStack>
+          <VerticalDivider
+            height="15px"
+            display={{ base: 'none', sm: 'block' }}
+          />
+          <Text fontSize="sm" color={textColor}>
+            100 Enrollments
+          </Text>
         </Stack>
 
         {/*  Badges */}
-        <Flex direction="row" justifyContent="center" mt={2} flexWrap="wrap">
+        <Flex direction="row" mt={0} flexWrap="wrap">
           <Badge icon={MdLanguage} data="English" size="sm" />
           <Badge icon={BsBarChartLine} data="Intermediate" size="sm" />
           <Badge icon={BsClock} data="01H:30M" size="sm" />
         </Flex>
-
-        <Button size="md" colorScheme="teal" variant="solid">
-          <NextLink
-            href={{
-              pathname: '/course/[slug]',
-              query: {
-                slug: 'react_js',
-              },
-            }}
-            passHref
-          >
-            <Link sx={{ 'text-decoration': 'none' }}>Start Learning...</Link>
-          </NextLink>
-        </Button>
       </Stack>
-    </div>
+
+      <NextLink
+        href={{
+          pathname: '/course/[slug]',
+          query: {
+            slug: 'react_js',
+          },
+        }}
+        passHref
+      >
+        <Button as={Link} size="md" colorScheme="teal" variant="solid">
+          Start Learning...
+        </Button>
+      </NextLink>
+    </Stack>
   )
 }
 
