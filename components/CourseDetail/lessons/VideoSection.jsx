2be86a7512a2bbdb81b86ae84e248ae3f6329ffe
Muhammad Ahmad <muhammad.ahmad8043@gmail.com>
2022-06-26 15:51:27 +0500

    Chore: Adjust video section width

--- components/CourseDetail/lessons/VideoSection.jsx
+++ components/CourseDetail/lessons/VideoSection.jsx
@@ -7,7 +7,7 @@ const VideoSection = () => {
   return (
     <Flex direction={{ base: 'column', lg: 'row' }} mb={{ base: 5, md: 1 }}>
       {/* Left Video Panel */}
-      <AspectRatio w={{ base: '100%', lg: '70%' }} ratio={18 / 9}>
+      <AspectRatio w={{ base: '100%', lg: '68%' }} ratio={18 / 9}>
         <iframe
           title="naruto"
           src="https://www.youtube.com/embed/QhBnZ6NPOY0"
@@ -17,9 +17,9 @@ const VideoSection = () => {
 
       {/* Right Lessons List */}
       <Stack
-        mt={{ base: 5, md: 0 }}
-        ml={{ base: 0, md: 5 }}
-        w={{ base: '100%', lg: '30%' }}
+        mt={{ base: 5, lg: 0 }}
+        ml={{ base: 0, lg: 5 }}
+        w={{ base: '100%', lg: '32%' }}
       >
         <LessonsSection lessons={lessons} />
       </Stack>
