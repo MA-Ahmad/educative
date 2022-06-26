ee9885348cbd62ebb207ac53cdaa0fbae6a3523f
Muhammad Ahmad <muhammad.ahmad8043@gmail.com>
2022-06-26 15:48:57 +0500

    Chore: Fix height accordion

--- components/CourseDetail/lessons/LessonsSection.jsx
+++ components/CourseDetail/lessons/LessonsSection.jsx
@@ -36,7 +36,7 @@ const LessonsSection = ({ lessons }) => {
         </Stack>
       </Box>
 
-      <Accordion defaultIndex={[0]} allowMultiple>
+      <Accordion defaultIndex={[0]} allowMultiple h="25rem" overflowY="scroll">
         {lessons.map((lesson, index) => (
           <AccordionItem key={index}>
             <h2>
