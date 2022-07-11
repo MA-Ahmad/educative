9355bbb5fdb5fb356724238ea59bfe71cf61b986
Muhammad Ahmad <muhammad.ahmad8043@gmail.com>
2022-07-11 22:24:08 +0500

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,62 +1,21 @@
-import { ChakraProvider, useColorMode } from "@chakra-ui/react";
-import Header from "../components/layouts/Header";
-import Footer from "../components/layouts/Footer";
-import theme from "../components/layouts/Theme";
-import { ClerkProvider } from "@clerk/nextjs";
-import "../clerk.css";
-import ProgressBar from "../components/Util/ProgressBar";
+import { ChakraProvider } from '@chakra-ui/react'
+import Header from '../components/layouts/Header'
+import Footer from '../components/layouts/Footer'
+import theme from '../components/layouts/Theme'
+import { ClerkProvider } from '@clerk/nextjs'
+import ProgressBar from '../components/Util/ProgressBar'
 
 function MyApp({ Component, pageProps }) {
-  const customThemeOptions_dark = {
-    general: {
-      fontColor: "#FFFFFF",
-      fontFamily:
-        "-apple-system,BlinkMacSystemFont,Segoe UI,Helvetica,Arial,sans-serif,Apple Color Emoji,Segoe UI Emoji,Segoe UI Symbol",
-    },
-    // buttons: {
-    //   fontColor: "#FFFFFF",
-    //   fontFamily:
-    //     "-apple-system,BlinkMacSystemFont,Segoe UI,Helvetica,Arial,sans-serif,Apple Color Emoji,Segoe UI Emoji,Segoe UI Symbol",
-    // },
-  };
-
-  const customThemeOptions_light = {
-    general: {
-      fontColor: "#1A202C",
-      fontFamily:
-        "-apple-system,BlinkMacSystemFont,Segoe UI,Helvetica,Arial,sans-serif,Apple Color Emoji,Segoe UI Emoji,Segoe UI Symbol",
-    },
-    // buttons: {
-    //   fontColor: "#1A202C",
-    //   fontFamily:
-    //     "-apple-system,BlinkMacSystemFont,Segoe UI,Helvetica,Arial,sans-serif,Apple Color Emoji,Segoe UI Emoji,Segoe UI Symbol",
-    // },
-  };
-  const { colorMode } = useColorMode();
-  console.log("color", colorMode);
-
-  // const themefun = () => {
-  //   if (colorMode == "dark") {
-  //     final_mode = customThemeOptions_dark;
-  //     return final_mode;
-  //   }
-
-  //   if (colorMode == "dark") {
-  //     final_mode = customThemeOptions_light;
-  //     return final_mode;
-  //   }
-  // };
-
   return (
     <ChakraProvider theme={theme}>
       <ProgressBar />
-      <ClerkProvider {...pageProps}>
-        <Header />
-        <Component {...pageProps} />
-        <Footer />
-      </ClerkProvider>
+      {/* <ClerkProvider {...pageProps}> */}
+      <Header />
+      <Component {...pageProps} />
+      <Footer />
+      {/* </ClerkProvider> */}
     </ChakraProvider>
-  );
+  )
 }
 
-export default MyApp;
+export default MyApp
